20a1c89c0a803c622e00936f5ed2a9a1d6021f0e
Kenny Capote Barrios <kcapo01@altec.local>
2019-01-04 11:00:26 -0300

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
 
-    urlDB = 'mongodb://kpote17:kpote17@ds017155.mlab.com:17155/cafe17';
+    urlDB = process.env.MONGO_URI;
 }
 
 
