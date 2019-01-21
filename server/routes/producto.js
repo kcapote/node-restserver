bfe2cf4041cdc58c241f72751c4beb8a0d2ed7b2
Kenny Capote Barrios <kcapo01@altec.local>
2019-01-21 11:55:46 -0300

    Productos y categorias

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -87,6 +87,44 @@ app.get('/productos/:id', verificaToken, (req, res) => {
 });
 
 
+
+//=================================
+//Buscar productos
+//==================================
+app.get('/productos/buscar/:termino', verificaToken, (req, res) => {
+
+    let termino = req.params.termino;
+
+    let regex = new RegExp(termino, 'i');
+
+
+    Producto.find({ nombre: regex })
+        .populate('categoria', 'descripcion')
+        .exec((err, productos) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            if (!productos) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productos
+            });
+
+        })
+
+})
+
+
 //=================================
 //Crear un producto 
 //==================================
@@ -140,14 +178,8 @@ app.put('/productos/:id', verificaToken, (req, res) => {
 
     let body = req.body;
 
-    let producto = {
-        nombre: body.nombre,
-        precioUni: body.precioUni,
-        descripcion: body.descripcion,
-        categoria: body.categoria,
-    };
+    Producto.findById(id, (err, productoDB) => {
 
-    Producto.findByIdAndUpdate(id, producto, { new: true, runValidators: true }, (err, productoDB) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
@@ -164,13 +196,64 @@ app.put('/productos/:id', verificaToken, (req, res) => {
             });
         }
 
-        res.json({
-            ok: true,
-            producto: productoDB
-        });
+        productoDB.nombre = body.nombre;
+        productoDB.precioUni = body.precioUni;
+        productoDB.categoria = body.categoria;
+        productoDB.disponible = body.disponible;
+        productoDB.descripcion = body.descripcion;
 
 
-    });
+        productoDB.save((err, productoGuardado) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            };
+            res.json({
+                ok: true,
+                producto: productoGuardado
+            });
+
+        })
+
+    })
+
+
+    // let producto = {
+    //     nombre: body.nombre,
+    //     precioUni: body.precioUni,
+    //     descripcion: body.descripcion,
+    //     categoria: body.categoria,
+    // };
+
+
+
+
+    // Producto.findByIdAndUpdate(id, producto, { new: true, runValidators: true }, (err, productoDB) => {
+    //     if (err) {
+    //         return res.status(500).json({
+    //             ok: false,
+    //             err
+    //         });
+    //     }
+
+    //     if (!productoDB) {
+    //         return res.status(400).json({
+    //             ok: false,
+    //             err: {
+    //                 message: 'El producto no existe'
+    //             }
+    //         });
+    //     }
+
+    //     res.json({
+    //         ok: true,
+    //         producto: productoDB
+    //     });
+
+
+    // });
 
 
 
