015b7838a0b3e327e5a0096b3ebad8b198c519b1
Kenny Capote <kenny.capote@gmail.com>
2019-01-23 00:03:14 -0300

    avances upload pro y usuarios

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -2,9 +2,16 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario')
+
+
 app.use(fileUpload());
 
-app.put('/upload', function(req, res) {
+app.put('/upload/:tipo/:id', function(req, res) {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
     if (!req.files) {
         return res.status(400).json({
             ok: false,
@@ -14,9 +21,45 @@ app.put('/upload', function(req, res) {
         });
     }
 
-    let archivo = req.files.archivo;
 
-    archivo.mv('uploads/filename.jpg', (err) => {
+
+    //Valida tipos
+    let tiposValidos = ['productos', 'usuarios'];
+    if(tiposValidos.indexOf( tipo ) < 0 ){
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son '+ tiposValidos.join(',')
+            }
+        })
+
+    } 
+    
+
+
+
+
+    let archivo = req.files.archivo;
+    let nombreCortado = archivo.name.split('.');
+    let extension = nombreCortado[ nombreCortado.length -1 ];
+    
+    //Extensiones permitidas
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg' ];
+
+    if(extensionesValidas.lastIndexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son '+ extensionesValidas.join(',')
+            }
+        })
+    } 
+
+    //Cambiar nombre al archivo
+    let nombreArchivo = `${ id }-${ new Date().getMilliseconds() }.${ extension }`
+
+
+    archivo.mv(`uploads/${ tipo }/${ nombreArchivo }`, (err) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
